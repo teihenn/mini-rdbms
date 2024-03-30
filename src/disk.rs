960df882695541c081d0037bf782c775f8f1025f
yo1956 <hm.hr.yossy@gmail.com>
2024-03-30 22:43:43 +0900

    [wip] Implement DiskManager

---
+++ src/disk.rs
@@ -0,0 +1,27 @@
+pub struct DiskManager {
+    heap_file: File,
+    next_page_id: u64,
+}
+
+impl DiskManager {
+    pub fn new(heap_file: File) -> io::Result<Self> {
+        let heap_file_size = heap_file.metadata()?.len();
+        let next_page_id = heap_file_size / PAGE_SIZE as u64;
+        Ok(Self {
+            heap_file,
+            next_page_id,
+        })
+    }
+
+    /// open a heap file and return a new disk manager
+    /// # Arguments
+    /// * `heap_file_path` - the path to the heap file
+    pub fn open(heap_file_path: impl AsRef<Path>) -> io::Result<Self> {
+        let heap_file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(heap_file_path)?;
+        Self::new(heap_file)
+    }
+}
